3f0dddf5fa9d5a9589ef91de922914f8365bdea7
Tanabata <52605305+Stranger31-20190707@users.noreply.github.com>
2021-10-05 12:20:08 +0900

    add error check

--- 2_3_4/mkreply.pl
+++ 2_3_4/mkreply.pl
@@ -16,6 +16,20 @@ while ($line = <>) {
         $from_name = $2;
     }
 }
+
+if (not defined($subject)) {
+    die "coudn't glean required infomation : subject"
+}
+if (not defined($date)) {
+    die "coudn't glean required infomation : date"
+}
+if (not defined($reply_address)) {
+    die "coudn't glean required infomation : reply_address"
+}
+if (not defined($from_name)) {
+    die "coudn't glean required infomation : from_name"
+}
+
 print "To: $reply_address ($from_name)\n";
 print "From: jfriedl\@regex.info (Jejjrey Friedl)\n";
 print "Subject: Re: $subject\n";
