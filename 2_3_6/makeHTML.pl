d619b5235adc9d34beb290497a4bbc7da78d939e
Stranger31-20190707 <52605305+Stranger31-20190707@users.noreply.github.com>
2021-12-11 16:02:09 +0900

    add URL substitution and fixed substitution result

--- 2_3_6/makeHTML.pl
+++ 2_3_6/makeHTML.pl
@@ -10,16 +10,27 @@ $text =~ s/>/&gt;/g;
 # $text =~ s/^$/<p>/mg
 $text =~ s/^\s*$/<p>/m; # /m:文字列を複数行として扱う修飾子
 
+$hostnameRegex = qr/[-a-z0-9]+(\.[-a-z0-9]+)*\.(com|edu|info)/;
+
 # メールアドレスを変換
 $text =~ s/
     \b(
         \w[-.\w]*
         \@
-        [-a-z0-9]+(\.[-a-z0-9]+)*\.(com|edu|info)
+        $hostnameRegex
     )\b
-    /
-    <a href="mailto:$1">$1<\/a>
-    /gix;
+    /<a href="mailto:$1">$1<\/a>/gix; # /xをつけても置換先は改行やスペースが有効になるので注意。
+
+# URLをリンクに変換
+$text =~ s/
+    \b(
+        https?:\/\/$hostnameRegex\b
+        (
+            \/[-a-z0-9_:\@&?=+,.!\/~*'^\$]*
+            (?![.,?!]) # 末尾が[.,?!]であってはならない
+        )?
+    )
+    /<a href="$1">$1<\/a>/gix;
 
 print $text;
 
